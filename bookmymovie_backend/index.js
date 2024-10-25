315b078fbfffaa8560b5be3c32f79bff9abc2520
Pratyushs411 <pratyushksk@gmail.com>
2024-10-25 20:42:34 +0530

    setup complete

--- bookmymovie_backend/index.js
+++ bookmymovie_backend/index.js
@@ -4,6 +4,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 8000;
+require('dotenv').config();
+require('./db')
 const cookieParser = require('cookie-parser');
 app.use(bodyParser.json());
 const allowedOrigins = ['http://localhost:3000'];
